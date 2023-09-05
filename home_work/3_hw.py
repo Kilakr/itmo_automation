65905177432bc0f841f2a47b9566b0b625289990
kilak <artemaur@gmail.com>
2023-09-05 16:13:27 +0300

    readme

---
+++ home_work/3_hw.py
@@ -0,0 +1,41 @@
+def func1 (x,y):
+    if x>y:
+        print(x)
+    elif x<y:
+        print (y)
+func1(3,5)
+def func2 (x,y):
+    if x-y == 135 or y-x == 135:
+        print('YES')
+    else:
+        print('NO')
+func2(136,1)
+def func3 (x):
+    if x in  (1,2,12):
+        print ('Зима')
+    elif x in range (3,6):
+        print ('Весна')
+    elif x in range (6,9):
+        print ('Лето')
+    elif x in range (9,12):
+        print ('Осень')
+func3(7)
+def func4(x,y,z):
+    if x>10 and y>10 and z>10:
+        print ('Yes')
+    else:
+        print ('No')
+func4(9,10,11)
+def func5(x):
+    a=0
+    for c in x:
+        if c>0:
+            a+=1
+    print(a)
+g=[1,2,-5,-4,5]
+func5(g)
+def func6(x,y):
+    c = 0
+    c = (x * 12 * 29) + (y*29)
+    print(c)
+func6(4,5)
---
+++ python_trening/task_8_if_elif.py
@@ -0,0 +1,63 @@
+num = 3
+
+if num >= 0:
+    print ('Число больше либо равно 0')
+else:
+    print ('Число меньше 0')
+
+num = -3
+
+if num >= 0:
+    print ('Число больше либо равно 0')
+else:
+    print ('Число меньше 0')
+
+num = 0
+
+if num >= 0:
+    print ('Число больше либо равно 0')
+else:
+    print ('Число меньше 0')
+
+str1 = 'cat'
+str2 = 'catt'
+
+if str1 in str2:
+    print ('YES')
+else:
+    print ('NO')
+
+num_float = 3.14
+
+if num_float > 0:
+    print ('Число положительное')
+elif num_float == 0:
+    print ('Число равно 0')
+else:
+    print ('Число отрицательное')
+
+num = 4
+permit_print = True
+
+if num > 0 and permit_print:
+    print ('num - положительное число')
+elif not permit_print:
+    print ('Печать запрещена')
+
+year = 5
+
+if year > 0 and year < 5:
+    print ('Бакалавр')
+elif year >= 5 and year < 7:
+    print ('Магистр')
+elif year >= 7 and year < 10:
+    print ('Аспирант')
+else:
+    print ('Введите корректный год обучения')
+
+number = 56
+if number not in range (-100, 101):
+    print('-')
+else:
+    print('+')
+
